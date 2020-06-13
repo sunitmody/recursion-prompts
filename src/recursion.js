a72e0a9cc7f1222fb5d7247142be78963f079c2b
Sunit Mody <sunitmody@gmail.com>
2020-06-12 18:04:59 -0700

    Implement palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -221,7 +221,29 @@ REVERSE COMPLETED
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  //make lowercase and remove space
+  string = string.split(' ').join('').toLowerCase();
+
+  //base case if palindrome
+  if (string.length === 0) {
+    return true;
+  }
+
+  //base case it not a palindrome (if the front and end characters dont match)
+  if (string[0] !== string[string.length-1]) {
+    return false;
+  }
+
+  //recursive case
+  return palindrome(string.slice(1,string.length-1));
 };
+
+/*
+########################################################
+PALINDROME COMPLETED
+########################################################
+*/
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
